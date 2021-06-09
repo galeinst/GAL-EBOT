29fd1944d6e2a4dc70b8720664dc18825098ca73
galeinst <81807946+galeinst@users.noreply.github.com>
2021-06-09 09:44:05 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,11 +97,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Lord-Userbot ERROR**\n"
-                    link = "Silahkan chat: @liualvinas"
+                    text = "**GAL-EBOT ERROR**\n"
+                    link = "Silahkan chat: @gstwne"
                     text += "Untuk melaporkan kesalahan"
                     text += f"- tinggal teruskan pesan ini {link}.\n"
-                    text += "Alvin Siap Membantu Kamu\n"
+                    text += "Gale Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
