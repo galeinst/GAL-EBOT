deb6c0d192c3627107d93c6d1737bdd52bcc567d
galeinst <81807946+galeinst@users.noreply.github.com>
2021-06-09 09:42:01 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"Lord-Userbot ⚙️ V{BOT_VER} [TELAH DIAKTIFKAN!]")
+    f"GAL-EBOT⚙️ V{BOT_VER} [UDAH AKTIF TOT!]")
 
 
 if len(argv) not in (1, 3, 4):
